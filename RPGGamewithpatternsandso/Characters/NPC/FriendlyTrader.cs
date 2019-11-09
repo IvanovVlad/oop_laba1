5fa5923af0242f9cd2151c3c07171a481765b92b
Vlad <iwanow.wld@gmail.com>
2019-11-09 20:58:23 +0400

    doc: Report docs

--- RPGGamewithpatternsandso/Characters/NPC/FriendlyTrader.cs
+++ RPGGamewithpatternsandso/Characters/NPC/FriendlyTrader.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 
 namespace RPGGamewithpatternsandso.Characters.NPC
 {
-    class FriendlyTrader : BaseCharacter
+    class FriendlyTrader : Friend
     {
-        public FriendlyTrader(ClassSpec @class, string name, int healthPoint, int armour, int attackPower) 
-            : base(@class, "GoodsTrader", healthPoint, armour, attackPower)
+        public FriendlyTrader(ClassSpec @class = ClassSpec.WARRIOR, string name = "Trader", int healthPoint = 100, int armour = 0, int attackPower = 0)
+            : base(@class, name, healthPoint, armour, attackPower)
         {
             attackBehaviour = new CharactersActions.NoAttack();
+            quest = new Quests.AssasinationQuest();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                resources.ItemList.Add(new Items.Armours.SteelArmour());
+                resources.ItemList.Add(new Items.Weapons.SimpleBow());
+                resources.ItemList.Add(new Items.Weapons.SteelSword());
+            }
         }
     }
 }
---
+++ RPGGamewithpatternsandso/Quests/TradeQuest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGGamewithpatternsandso.Quests
+{
+    class TradeQuest : QuestBase
+    {
+        public TradeQuest(QuestType qType = QuestType.TRADE, string name = "Trade",
+            string text = "Hello! Sell me some thing", int goldReward = 10, int expReward = 10)
+            : base(qType, name, text, goldReward, expReward)
+        {
+        }
+    }
+}
